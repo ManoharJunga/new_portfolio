5e421eb206cc879c088c6b5322057d3e087d96c3
ManoharJunga <95352064+ManoharJunga@users.noreply.github.com>
2024-06-17 09:30:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Navbar from './components/Navbar';
 import Home from './Pages/Main';
 import About from './Pages/About';
 import Projects from './Pages/Projects';
 import Contact from './Pages/Contact';
-import './Pages/PageTransition.css';
-
 function App() {
   const location = useLocation();
 
   return (
     <div className="App">
       <Navbar />
-      <TransitionGroup>
-        <CSSTransition key={location.key} classNames="page" timeout={300}>
           <Routes location={location}>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
-        </CSSTransition>
-      </TransitionGroup>
     </div>
   );
 }
