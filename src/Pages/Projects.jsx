2ec9a7e53f5477845902ecf7695def7543d10524
ManoharJunga <95352064+ManoharJunga@users.noreply.github.com>
2024-06-18 12:33:54 +0530

    commit

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import './Projects.css'
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className="home__hero">
+      <div className="home__hero__text center" data-mobile="">
+        <p className="oh">Few of my</p>
+      </div>
+      <div className="home__hero__text center" data-mobile="">
+        <p className="oh">Amazing</p>
+      </div>
+      <div className="home__hero__text center" data-mobile="">
+        <p className="oh project">Projects</p>
+      </div>
+    </div>
   )
 }
 
