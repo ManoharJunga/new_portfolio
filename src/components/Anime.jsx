e1627d359d1e6c434539ac1fcd2be0933d463dba
ManoharJunga <95352064+ManoharJunga@users.noreply.github.com>
2024-06-19 21:56:04 +0530

    commit

--- src/components/Anime.jsx
+++ src/components/Anime.jsx
@@ -22,55 +22,49 @@ function Anime() {
         className="anime-image"
         style={{ transform: `translateY(-${offsetY * 0.2}px)` }}
       />
-
-<div className='marquees'>
-<section className='marquee' style={{ '--char-count': 24 }}>
-    <div className='marquee--inner'>
-      <p>Learning new technologies and solving complex problems</p>
-      <p aria-hidden='true'>Learning new technologies and solving complex problems</p>
-      <p aria-hidden='true'>Learning new technologies and solving complex problems</p>
-      <p aria-hidden='true'>Learning new technologies and solving complex problems</p>
-      <p aria-hidden='true'>Learning new technologies and solving complex problems</p>
-      <p aria-hidden='true'>Learning new technologies and solving complex problems</p>
-    </div>
-  </section>
-
-  <div className='marquee' style={{ '--char-count': 30 }}>
-    <div className='marquee--inner'>
-      <p>React Express Node.js HTML Tailwind Mongo</p>
-      <p aria-hidden='true'>React Express Node.js HTML Tailwind Mongo</p>
-      <p aria-hidden='true'>React Express Node.js HTML Tailwind Mongo</p>
-      <p aria-hidden='true'>React Express Node.js HTML Tailwind Mongo</p>
-      <p aria-hidden='true'>React Express Node.js HTML Tailwind Mongo</p>
-      <p aria-hidden='true'>React Express Node.js HTML Tailwind Mongo</p>
-      <p aria-hidden='true'>React Express Node.js HTML Tailwind Mongo</p>
-    </div>
-  </div>
-
-  <section className='marquee' style={{ '--char-count': 22 }}>
-    <div className='marquee--inner'>
-      <p>I have experience in building responsive and scalable web applications</p>
-      <p aria-hidden='true'>I have experience in building responsive and scalable web applications</p>
-      <p aria-hidden='true'>I have experience in building responsive and scalable web applications</p>
-      <p aria-hidden='true'>I have experience in building responsive and scalable web applications</p>
-      <p aria-hidden='true'>I have experience in building responsive and scalable web applications</p>
-      <p aria-hidden='true'>I have experience in building responsive and scalable web applications</p>
-    </div>
-  </section>
-
-  <section className='marquee' style={{ '--char-count': 24 }}>
-    <div className='marquee--inner'>
-      <p>I am passionate about learning new technologies and solving complex problems</p>
-      <p aria-hidden='true'>I am passionate about learning new technologies and solving complex problems</p>
-      <p aria-hidden='true'>I am passionate about learning new technologies and solving complex problems</p>
-      <p aria-hidden='true'>I am passionate about learning new technologies and solving complex problems</p>
-      <p aria-hidden='true'>I am passionate about learning new technologies and solving complex problems</p>
-      <p aria-hidden='true'>I am passionate about learning new technologies and solving complex problems</p>
-    </div>
-  </section>
-</div>
-
-
+      <div className='marquees'>
+        <section className='marquee' style={{ '--char-count': 24 }}>
+          <div className='marquee--inner'>
+            <p>Learning new technologies and solving complex problems</p>
+            <p aria-hidden='true'>Learning new technologies and solving complex problems</p>
+            <p aria-hidden='true'>Learning new technologies and solving complex problems</p>
+            <p aria-hidden='true'>Learning new technologies and solving complex problems</p>
+            <p aria-hidden='true'>Learning new technologies and solving complex problems</p>
+            <p aria-hidden='true'>Learning new technologies and solving complex problems</p>
+          </div>
+        </section>
+        <section className='marquee' style={{ '--char-count': 30 }}>
+          <div className='marquee--inner'>
+            <p>React Express Node.js HTML Tailwind Mongo</p>
+            <p aria-hidden='true'>React Express Node.js HTML Tailwind Mongo</p>
+            <p aria-hidden='true'>React Express Node.js HTML Tailwind Mongo</p>
+            <p aria-hidden='true'>React Express Node.js HTML Tailwind Mongo</p>
+            <p aria-hidden='true'>React Express Node.js HTML Tailwind Mongo</p>
+            <p aria-hidden='true'>React Express Node.js HTML Tailwind Mongo</p>
+            <p aria-hidden='true'>React Express Node.js HTML Tailwind Mongo</p>
+          </div>
+        </section>
+        <section className='marquee' style={{ '--char-count': 22 }}>
+          <div className='marquee--inner'>
+            <p>I have experience in building responsive and scalable web applications</p>
+            <p aria-hidden='true'>I have experience in building responsive and scalable web applications</p>
+            <p aria-hidden='true'>I have experience in building responsive and scalable web applications</p>
+            <p aria-hidden='true'>I have experience in building responsive and scalable web applications</p>
+            <p aria-hidden='true'>I have experience in building responsive and scalable web applications</p>
+            <p aria-hidden='true'>I have experience in building responsive and scalable web applications</p>
+          </div>
+        </section>
+        <section className='marquee' style={{ '--char-count': 24 }}>
+          <div className='marquee--inner'>
+            <p>I am passionate about learning new technologies and solving complex problems</p>
+            <p aria-hidden='true'>I am passionate about learning new technologies and solving complex problems</p>
+            <p aria-hidden='true'>I am passionate about learning new technologies and solving complex problems</p>
+            <p aria-hidden='true'>I am passionate about learning new technologies and solving complex problems</p>
+            <p aria-hidden='true'>I am passionate about learning new technologies and solving complex problems</p>
+            <p aria-hidden='true'>I am passionate about learning new technologies and solving complex problems</p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
